d6b61217acb84a1633f9f4fd3636a78270212bfd
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-05-24 22:04:25 +0800

    UPDATE BASE

--- nonilon_portfolio/settings.py
+++ nonilon_portfolio/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-             'templates'
+             BASE_DIR / 'templates'
             ],
         'APP_DIRS': True,
         'OPTIONS': {
